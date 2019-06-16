4faebee2bb2a8ee60b298b0a9b2ab38c7889ecf6
edgarbs1998 <edgarbs1998@users.noreply.github.com>
2019-06-16 20:39:30 +0100

    Added files without being ignored

---
+++ build/temp/development/TaskList/sass/config.rb
@@ -0,0 +1,1 @@
+cache_path = 'C:\Users\esantos98\Workspace\python-tasklist-client/build/.sass-cache'
---
+++ build/temp/development/TaskList/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require C:\Users\esantos98\Workspace\python-tasklist-client\app.js
