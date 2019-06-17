606b02280a8ea61f59ca2f2abf70259530c5d9f2
edgarbs1998 <edgarbs1998@users.noreply.github.com>
2019-06-17 22:08:19 +0100

    Removed ignored files

--- build/temp/development/TaskList/sencha-compiler/app/full-page-master-bundle.js
+++
@@ -1,2 +0,0 @@
-// @tag full-page
-// @require C:\Users\esantos98\Workspace\python-tasklist-client\app.js
--- build/temp/production/TaskList/sass/config.rb
+++
@@ -1,1 +0,0 @@
-cache_path = 'C:\Users\esantos98\Workspace\python-tasklist-client/build/.sass-cache'
